9a9586d179c86f8b86dc107fb536e6bf78556f0d
hikitae <hikitae@gmail.com>
2017-05-01 15:40:23 -0700

    First draft of JS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var interval = window.setInterval(rotateSlides, 5000)
+  
+  function rotateSlides(){
+    var $firstSlide = $("#mainimage").find(".slide-image:first");
+    var width = $firstSlide.width();
+    
+    $firstSlide.animate({marginLeft: -width}, 2000, function(){
+      var $lastSlide = $("#mainimage").find(".slide-image:last")
+      $lastSlide.after($firstSlide);
+      $firstSlide.css({marginLeft: 0})
+    });
+  }
+
+});
+
+
+
