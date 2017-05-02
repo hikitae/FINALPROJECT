7a2a0b67ce8fc8358d60ea5c223f4da9db971eb2
hikitae <hikitae@gmail.com>
2017-05-01 18:27:55 -0700

    Css edited

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-  var interval = window.setInterval(rotateSlides, 5000)
+// $(document).ready(function(){
+//   var interval = window.setInterval(rotateSlides, 5000)
   
-  function rotateSlides(){
-    var $firstSlide = $("#mainimage").find(".slide-image:first");
-    var width = $firstSlide.width();
+//   function rotateSlides(){
+//     var $firstSlide = $("#mainimage").find(".slide-image:first");
+//     var width = $firstSlide.width();
     
-    $firstSlide.animate({marginLeft: -width}, 2000, function(){
-      var $lastSlide = $("#mainimage").find(".slide-image:last")
-      $lastSlide.after($firstSlide);
-      $firstSlide.css({marginLeft: 0})
-    });
-  }
+//     $firstSlide.animate({marginLeft: -width}, 2000, function(){
+//       var $lastSlide = $("#mainimage").find(".slide-image:last")
+//       $lastSlide.after($firstSlide);
+//       $firstSlide.css({marginLeft: 0})
+//     });
+//   }
 
-});
+// });
 
 
 
