a77a406f023118efac5b1e08681bda2ec68ef5bd
hikitae <hikitae@gmail.com>
2017-05-03 18:41:55 -0700

    Gallery contact edited

--- js/script.js
+++ js/script.js
@@ -1,18 +1,95 @@
 $(document).ready(function(){
-  var interval = window.setInterval(rotateSlides, 5000)
+//   var interval = window.setInterval(rotateSlides, 5000)
   
-  function rotateSlides(){
-    var $firstSlide = $("#mainimage").find(".slide-image:first");
-    var width = $firstSlide.width();
+//   function rotateSlides(){
+//     var $firstSlide = $("#mainimage").find(".slide-image:first");
+//     var width = $firstSlide.width();
     
-    $firstSlide.animate({marginLeft: -width}, 2000, function(){
-      var $lastSlide = $("#mainimage").find(".slide-image:last")
-      $lastSlide.after($firstSlide);
-      $firstSlide.css({marginLeft: 0})
-    });
-  }
+//     $firstSlide.animate({marginLeft: -width}, 2000, function(){
+//       var $lastSlide = $("#mainimage").find(".slide-image:last")
+//       $lastSlide.after($firstSlide);
+//       $firstSlide.css({marginLeft: 0})
+//     });
+//   }
+
+// });
+
+	$('#left-arrow').click(previousSlide);
+	$('#right-arrow').click(nextSlide);
+	$('.slide-image').click(nextSlide);
+
+	function nextSlide(){
+	  window.clearInterval(interval);
+	  var $currentSlide = $('#mainimage').find('.slide-image:first');
+	  var width = $currentSlide.width();
+	  $currentSlide.animate({marginLeft: -width}, 1000, function(){
+	    var $lastSlide = $('#mainimage').find('.slide-image:last')
+	    $lastSlide.after($currentSlide);
+	    $currentSlide.css({marginLeft: 0})
+	    interval = window.setInterval(rotateSlides, 3000);
+	  });
+	}
+
+	function previousSlide(){
+	  window.clearInterval(interval);
+	  var $currentSlide = $('#mainimage').find('.slide-image:first');
+	  var width = $currentSlide.width();
+	  var $previousSlide = $('#mainimage').find('.slide-image:last')
+	  $previousSlide.css({marginLeft: -width})
+	  $currentSlide.before($previousSlide);
+	  $previousSlide.animate({marginLeft: 0}, 1000, function(){
+	    interval = window.setInterval(rotateSlides, 3000);
+	  });
+
+	}
+
+
+	// Get the modal
+	// var modal = document.getElementById('myModal');
+	var modal =$("#myModal");
+	// Get the button that opens the modal
+	// var btn = document.getElementById("VRvideo");
+	$("#btn-wrapper img").click(function(){
+		console.log("image clicked...");
+		modal.show();
+
+	})
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	// btn.onclick = function() {
+	// 	console.log("modal clicked");
+	//     modal.style.display = "block";
+	//  }
+
+	// When the user clicks on <span> (x), close the modal
+	// span.onclick = function() {
+	//     modal.style.display = "none";
+	// }
+
+	// var span = $(".close");
+	$(".close").click(function(){
+		console.log("image closed...")
+		modal.hide();
+	});
+
+	// When the user clicks anywhere outside of the modal, close it
+	// window.onclick = function(event) {
+	//     if (event.target == modal) {
+	//         modal.style.display = "none";
+	//     }
+	// }
+
+
+
+
 
 });
 
+
+
+	
 
 
